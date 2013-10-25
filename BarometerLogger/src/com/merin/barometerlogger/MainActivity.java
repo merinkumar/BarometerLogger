cbf2ba8ad912748d2a001eb1ceb78751955d16ad
merin <talktomerin@gmail.com>
2013-10-25 13:44:26 -0400

    basic skeleton - not working yet

--- BarometerLogger/src/com/merin/barometerlogger/MainActivity.java
+++ BarometerLogger/src/com/merin/barometerlogger/MainActivity.java
@@ -1,15 +1,39 @@
 package com.merin.barometerlogger;
 
-import android.os.Bundle;
+import java.util.List;
+
+import org.xml.sax.Parser;
+
 import android.app.Activity;
+import android.app.Service;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
 
+	private SensorManager mSensorManager;
+	private Sensor mSensor;
+	private int mdelay;
+	private TextView tv2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		tv2 = (TextView) findViewById(android.R.id.text2);
+		
+		mSensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+		//List<Sensor> deviceSensor = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+		if(mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
+			mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+		}
+		mdelay = mSensor.getMinDelay();
+		mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	@Override
@@ -17,6 +41,19 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float p =  event.values[0];
+		tv2.setText(p);
+		
 	}
 
 }
